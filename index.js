e4142e4aa6db3bb18916d0030a49959190942052
54 Bridge <54bridgeapp@gmail.com>
2020-01-03 15:12:53 -0600

    added response to client

--- index.js
+++ index.js
@@ -13,14 +13,25 @@ app.post("/", (req, res) => {
   if (!req.body) {
     return res.status(400).json("Values cannot be empty");
   }
-  request(
-    "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD",
-    function(error, response, body) {
-      console.log("error:", error); // Print the error if one occurred
-      console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-      console.log("body:", body); // Print the HTML for the Google homepage.
+  const coin = req.body.crypto;
+  const currency = req.body.fiat;
+  const url = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+  const apiUrl = url + coin + currency;
+  request(apiUrl, function(error, response, body) {
+    if (error) {
+      return res.status(400).json("Something went wrong");
     }
-  );
+
+    const data = JSON.parse(body);
+    const price = data.last;
+
+    const currentDate = data.display_timestamp;
+
+    res.write(`<p>The current date is ${currentDate}</p>`);
+    res.write(`<h1> The current price of ${coin} is ${price} ${currency}</h1>`);
+
+    res.send();
+  });
 });
 
 app.listen(3000, () => {
